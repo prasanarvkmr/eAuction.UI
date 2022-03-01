681224e62cf2ff6d38ad8f0f7f75cba4e4ff9ba8
Prasana Ravi <prasana.ravikumar@yahoo.com>
2022-03-01 11:02:07 +0530

    Ui Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isUserAvailable:boolean = false;;
+  constructor(private apiService: ApiService){}
+
+  ngOnInit() {
+    this.isUserAvailable = this.apiService.loadUserData();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes)
+}
+
   title = 'ui-app';
 }
